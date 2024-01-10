43926104d36c2c3530ca6ff1bf0ac23ef5ed99e9
gadgelogger <sarayou273142910@gmail.com>
2024-01-11 01:07:59 +0900

    Initial Commit

---
+++ MyMusic/MyMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusicApp.swift
+//  MyMusic
+//
+//  Created by gadgelogger on 2024/01/11.
+//
+
+import SwiftUI
+
+@main
+struct MyMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
